179662dd7125695e83f78dd84fca92bd44a1c52b
Athantor <athantor@gmail.com>
2009-06-14 04:35:19 +0200

    new class

---
+++ src/libfarec/Classifier.hh
@@ -0,0 +1,33 @@
+/*
+ *  This file is part of farec.
+ *
+ *  farec is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  farec is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with farec.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Classifier.hh
+ *
+ *  Created on: 2009-06-14
+ *      Author: athantor
+ */
+
+#ifndef CLASSIFIER_HH_
+#define CLASSIFIER_HH_
+
+class Classifier
+{
+	public:
+		Classifier();
+		virtual ~Classifier();
+};
+
+#endif /* CLASSIFIER_HH_ */
